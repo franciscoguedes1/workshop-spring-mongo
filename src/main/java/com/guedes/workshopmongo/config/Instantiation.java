54837e84713350f36b6a752f038571041017841f
franciscoguedes1 <guedes7481@hotmail.com>
2021-10-13 11:45:32 -0300

    projecao dos dados do author com DTO

--- src/main/java/com/guedes/workshopmongo/config/Instantiation.java
+++ src/main/java/com/guedes/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.guedes.workshopmongo.domain.Post;
 import com.guedes.workshopmongo.domain.User;
+import com.guedes.workshopmongo.dto.AuthorDTO;
 import com.guedes.workshopmongo.repository.PostRepository;
 import com.guedes.workshopmongo.repository.UserRepository;
 
@@ -35,13 +36,16 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1= new Post(null,sdf.parse("21/03/2018"),"Partiu viagem", 
-				"Vou viajar para São Paulo. Abraços!",maria);
+		  // salvando no banco de dados 
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
 		
-		Post post2=new Post(null,sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hoje!",maria);
+		Post post1= new Post(null,sdf.parse("21/03/2018"),"Partiu viagem", 
+				"Vou viajar para São Paulo. Abraços!",new AuthorDTO(maria));
+		
+		Post post2=new Post(null,sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hoje!"
+				,new AuthorDTO(maria));
 				
 			// salvando no banco de dados 	
-		userRepository.saveAll(Arrays.asList(maria,alex,bob));
 		postRepository.saveAll(Arrays.asList(post1,post2));
 	}
 
