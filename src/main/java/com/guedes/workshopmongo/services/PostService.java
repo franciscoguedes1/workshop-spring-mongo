85f4c47f3764201fa47971809ac774eae6d7ca10
franciscoguedes1 <guedes7481@hotmail.com>
2021-10-13 12:57:55 -0300

    obtendo um post por id

---
+++ src/main/java/com/guedes/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.guedes.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guedes.workshopmongo.domain.Post;
+import com.guedes.workshopmongo.repository.PostRepository;
+import com.guedes.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
