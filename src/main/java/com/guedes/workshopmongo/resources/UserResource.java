f16e3880bd1789ff84fd17b70b410b258cde54ab
franciscoguedes1 <guedes7481@hotmail.com>
2021-10-13 12:39:32 -0300

    endpoint para retornar os posts do usuário

--- src/main/java/com/guedes/workshopmongo/resources/UserResource.java
+++ src/main/java/com/guedes/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.guedes.workshopmongo.domain.Post;
 import com.guedes.workshopmongo.domain.User;
 import com.guedes.workshopmongo.dto.UserDTO;
 import com.guedes.workshopmongo.services.UserService;
@@ -62,6 +63,13 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	//Endpoint para retornar os posts de um usuário
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
